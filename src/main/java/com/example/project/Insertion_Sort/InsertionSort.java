8e7eae00a380c459f27ba6ef3dd73755b260ea40
jasnli <167535698+jasnli@users.noreply.github.com>
2025-02-26 13:48:19 +0000

    messahe

--- src/main/java/com/example/project/Insertion_Sort/InsertionSort.java
+++ src/main/java/com/example/project/Insertion_Sort/InsertionSort.java
@@ -5,9 +5,31 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        insertionSort(arr);
+        System.out.println();
+        for (int i : arr) {
+            System.out.print(i + ", ");
+        }
+    }
 
     // PART A. implementing insertion sort
     public static int[] insertionSort(int[] elements) {
+        for (int i = 1; i < elements.length; i++) {
+            int inthedex = i;
+            System.out.println("ITD: " + inthedex);
+            while (inthedex >= 1) {
+                if (elements[inthedex] < elements[inthedex - 1]) {
+                    System.out.println(elements[inthedex]);
+                    int temp = elements[inthedex - 1];
+                    elements[inthedex - 1] = elements[inthedex];
+                    elements[inthedex] = temp;   
+                }
+                inthedex --;
+                
+            }
+        }
         return elements;
     }
 
@@ -18,6 +40,18 @@ public class InsertionSort {
 
     // PART B. sorting a 1000-word list
     public static ArrayList<String> insertionSortWordList(ArrayList<String> words) {
+        for (int i = 1; i < words.size(); i++) {
+            int inthedex = i;
+            System.out.println("ITD: " + inthedex);
+            while (inthedex >= 1) {
+                if (words.get(inthedex).compareTo(words.get(inthedex - 1)) < 0) {
+                    System.out.println(words.get(inthedex));
+                    words.set(inthedex, words.set(inthedex - 1, words.get(inthedex)));
+                }
+                inthedex --;
+                
+            }
+        }
         return words;
     }
 
